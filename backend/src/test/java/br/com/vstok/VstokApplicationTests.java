883b85e66a335ea27339b5bbf5d5b06f8efc5140
Victor Silva <vitinhogsoo@gmail.com>
2024-01-18 12:42:57 -0300

    initial backend server

---
+++ backend/src/test/java/br/com/vstok/VstokApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vstok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VstokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
