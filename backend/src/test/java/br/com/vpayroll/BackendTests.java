f662478711342bf970a6eddd343eb1bbb8068c2f
Victor Silva <vitinhogsoo@gmail.com>
2024-01-20 16:14:37 -0300

    backend adjusts

--- backend/src/test/java/br/com/vpayroll/BackendTests.java
+++ backend/src/test/java/br/com/vpayroll/BackendTests.java
@@ -1,10 +1,10 @@
-package br.com.vstok;
+package br.com.vpayroll;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VstokApplicationTests {
+class BackendTests {
 
 	@Test
 	void contextLoads() {
